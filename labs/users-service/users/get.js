e9d69f038f08aac1dadf28c677b7fc03dd96dbc4
Rupak Ganguly <rupakg@gmail.com>
2017-10-30 23:38:17 -0400

    Add logic to simulate error.

--- labs/users-service/users/get.js
+++ labs/users-service/users/get.js
@@ -2,26 +2,39 @@
 
 module.exports.getUser = (event, context, callback) => {
 
-  console.log("*** Input: "+JSON.stringify(event)+" ***\n");
-
   // process incoming input data
   var id = JSON.parse(event.pathParameters.id);
 
-  // fetch the user
-  var data = {
-    id: id,
-    name: "John Doe",
-    email: "john.doe@email.com",
+  // simulate an error
+  if (id === 999) {
+    // send error response
+    const err_resp = {
+      statusCode: 404,
+      body: JSON.stringify({
+        message: 'No user found.',
+      }),
+    };
+    // log the error
+    console.log("*** Error: "+JSON.stringify(err_resp)+" ***\n");
+    callback(null, err_resp);
+
+  } else {
+
+    // fetch the user
+    var data = {
+      id: id,
+      name: "John Doe",
+      email: "john.doe@email.com",
+    }
+
+    // send response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: data,
+        message: 'User fetched successfully.',
+      }),
+    };
+    callback(null, response);
   }
-
-  // send response
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: data,
-      message: 'User fetched successfully.',
-    }),
-  };
-
-  callback(null, response);
 };
